c963bc9f793baf4dc486a9059a1263cec985cd63
PeterKey <PeterKey@users.noreply.github.com>
2019-04-19 19:56:48 +0200

    Preconfiguration for AWS

---
+++ server.compiled.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = process.env.HTTP_PORT || 4001;
+const app = express();
+app.get('/', (req, res) => {
+  res.send('flowers smell nice');
+});
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}.`);
+});
