6985eb9b7af22fef30d70d7c5e8607afdb8c0c5f
PeterKey <p.korcsak@googlemail.com>
2019-09-27 15:26:20 +0200

    Create Functions.js

---
+++ src/components/Functions.js
@@ -0,0 +1,16 @@
+import queryString from 'query-string'
+import { history } from '../configureStore'
+
+export function selectPetal (petal) {
+  const parsed = queryString.parse(history.location.search)
+
+  const id = (petal) ? petal.id : undefined
+
+  if (parseInt(parsed.s) !== id) {
+    if (id) {
+      history.push({ search: `s=${id}` })
+    } else {
+      history.push({ search: '' })
+    }
+  }
+}
