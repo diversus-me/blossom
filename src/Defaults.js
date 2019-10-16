c2ffe93a3a99daec45c28558564370663a39d89e
maaz28 <maazjawaid28@gmail.com>
2019-10-16 12:14:17 +0500

    Font added, searchbar styling, card styling, horizontal scroll remove from sidebar

--- src/Defaults.js
+++ src/Defaults.js
@@ -1,60 +1,60 @@
-import { MdNotInterested } from 'react-icons/md'
-import { GoBeaker } from 'react-icons/go'
-import { FaLaughBeam, FaBalanceScale } from 'react-icons/fa'
-import { IoIosCheckmarkCircle, IoIosHammer } from 'react-icons/io'
+import { MdNotInterested } from "react-icons/md";
+import { GoBeaker } from "react-icons/go";
+import { FaLaughBeam, FaBalanceScale } from "react-icons/fa";
+import { IoIosCheckmarkCircle, IoIosHammer } from "react-icons/io";
 
-export const MARKER_SIZE = 20
+export const MARKER_SIZE = 20;
 
-export const DOWN_SCALE_FACTOR = 0.6
-export const LOST_PETAL_DOWN_SCALE_FACTOR = 1
+export const DOWN_SCALE_FACTOR = 0.6;
+export const LOST_PETAL_DOWN_SCALE_FACTOR = 1;
 
-export const MAGNIFY_SPEED = 600
-export const UNMAGNIFY_SPEED = 800
+export const MAGNIFY_SPEED = 600;
+export const UNMAGNIFY_SPEED = 800;
 
-export const NAVBAR_HEIGHT = 60
-export const SIDEBAR_WIDTH = 320
+export const NAVBAR_HEIGHT = 60;
+export const SIDEBAR_WIDTH = 320;
 
 export const FLAVORS = [
   {
-    name: 'Neutral',
-    type: 'neutral',
-    color: '#979ca6',
+    name: "Neutral",
+    type: "neutral",
+    color: "#979ca6",
     icon: FaBalanceScale
   },
   {
-    name: 'Pro',
-    type: 'pro',
-    color: '#4b8a6e',
+    name: "Pro",
+    type: "pro",
+    color: "#4b8a6e",
     icon: IoIosCheckmarkCircle
   },
   {
-    name: 'Contra',
-    type: 'contra',
-    color: '#ff2b4d',
+    name: "Contra",
+    type: "contra",
+    color: "#ff2b4d",
     icon: MdNotInterested
   },
   {
-    name: 'Science',
-    type: 'science',
-    color: '#496f8e',
+    name: "Science",
+    type: "science",
+    color: "#496f8e",
     icon: GoBeaker
   },
   {
-    name: 'Joke',
-    type: 'joke',
-    color: '#ffe761',
+    name: "Joke",
+    type: "joke",
+    color: "#ffe761",
     icon: FaLaughBeam
   },
   {
-    name: 'Fact Check',
-    type: 'fact check',
-    color: '#457ece',
+    name: "Fact Check",
+    type: "fact check",
+    color: "#457ece",
     icon: IoIosHammer
   }
-]
+];
 
-export function getFlavor (type) {
-  return FLAVORS.find((element) => {
-    return element.type === type
-  })
+export function getFlavor(type) {
+  return FLAVORS.find(element => {
+    return element.type === type;
+  });
 }
