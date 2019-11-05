12ee300f13a16410c22c309c2e6d938c62401533
hameed <vjhameed3@gmail.com>
2019-11-05 07:48:51 -0800

    fix

--- src/components/FlowerView.js
+++ src/components/FlowerView.js
@@ -1,55 +1,55 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
-import { toast } from 'react-toastify'
-import 'react-tiny-fab/dist/styles.min.css'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { toast } from "react-toastify";
+import "react-tiny-fab/dist/styles.min.css";
 
-import { getFlowerData } from '../state/flowerData/actions'
-import { startEditNodeRoutine } from '../state/globals/actions'
-import { NAVBAR_HEIGHT, SIDEBAR_WIDTH } from '../Defaults'
+import { getFlowerData } from "../state/flowerData/actions";
+import { startEditNodeRoutine } from "../state/globals/actions";
+import { NAVBAR_HEIGHT, SIDEBAR_WIDTH } from "../Defaults";
 
-import FlowerRenderer from './Flower/FlowerRenderer'
-import AddNodeRoutine from './Routines/NodeRoutine'
-import SeedInfo from './FlowerUI/SeedInfo'
-import PetalInfo from './FlowerUI/PetalInfo'
-import ActionButtonSimple from './UI/ActionButtonSimple'
+import FlowerRenderer from "./Flower/FlowerRenderer";
+import AddNodeRoutine from "./Routines/NodeRoutine";
+import SeedInfo from "./FlowerUI/SeedInfo";
+import PetalInfo from "./FlowerUI/PetalInfo";
+import ActionButtonSimple from "./UI/ActionButtonSimple";
 
-import style from './FlowerView.module.css'
+import style from "./FlowerView.module.css";
 
 class FlowerView extends React.Component {
   state = {
     showHandles: false,
-    addNodeType: ''
+    addNodeType: ""
   };
 
   currentTime = 0;
   currentProgress = 0;
 
-  componentDidMount () {
-    const { id, flowerData } = this.props
+  componentDidMount() {
+    const { id, flowerData } = this.props;
 
     if (!flowerData[id]) {
-      this.props.getFlowerData(id)
+      this.props.getFlowerData(id);
     }
   }
 
-  componentDidUpdate () {
-    const { id, flowerData } = this.props
+  componentDidUpdate() {
+    const { id, flowerData } = this.props;
 
     if (!flowerData[id]) {
-      this.props.getFlowerData(id)
+      this.props.getFlowerData(id);
     }
   }
 
   delete = () => {
-    const { flowerData, globals } = this.props
+    const { flowerData, globals } = this.props;
     if (window.confirm(`Are you sure you want to delete the selected Node?`)) {
       fetch(`${process.env.REACT_APP_SERVER_URL}/api/node`, {
-        credentials: 'include',
-        method: 'DELETE',
+        credentials: "include",
+        method: "DELETE",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
           id: flowerData[globals.selectedFlower].connections.find(
@@ -59,32 +59,32 @@ class FlowerView extends React.Component {
       })
         .then(response => {
           if (response.ok) {
-            return response
+            return response;
           } else {
-            throw new Error('failed')
+            throw new Error("failed");
           }
         })
-        .then(() => toast.success('Node successfully deleted'))
+        .then(() => toast.success("Node successfully deleted"))
         // TODO: Why does reloading the flower instantly after deleting cause wrong responses?
         .then(setTimeout(this.props.getFlowerData(globals.selectedFlower), 300))
         .catch(() => {
-          toast.error('Node could not be deleted.')
-        })
+          toast.error("Node could not be deleted.");
+        });
     }
   };
 
   setCurrentTime = (time, progress) => {
-    this.currentTime = time
-    this.currentProgress = progress
+    this.currentTime = time;
+    this.currentProgress = progress;
   };
 
   setHandles = show => {
     this.setState({
       showHandles: show
-    })
+    });
   };
 
-  render () {
+  render() {
     const {
       id,
       flowerData,
@@ -92,18 +92,18 @@ class FlowerView extends React.Component {
       dimensions,
       sideBarOpen,
       globals: { addNodeRoutineRunning, editNodeRoutineRunning, selectedPetal }
-    } = this.props
-    const { addNodePos } = this.state
-    const data = flowerData[id]
+    } = this.props;
+    const { addNodePos } = this.state;
+    const data = flowerData[id];
 
-    let selectedPetalData
+    let selectedPetalData;
     if (data && data.finished) {
       selectedPetalData = data.connections.find(
         connection => connection.id === selectedPetal
-      )
+      );
     }
 
-    const nodeRoutineRunning = addNodeRoutineRunning || editNodeRoutineRunning
+    const nodeRoutineRunning = addNodeRoutineRunning || editNodeRoutineRunning;
 
     return (
       <div
@@ -120,22 +120,22 @@ class FlowerView extends React.Component {
             transform:
               dimensions.safeToMove && sideBarOpen && !nodeRoutineRunning
                 ? `translateX(${Math.floor(SIDEBAR_WIDTH * 0.5)}px)`
-                : 'translateX(0)'
+                : "translateX(0)"
           }}
         >
           {session.authenticated &&
             data &&
             data.connections &&
             nodeRoutineRunning && (
-            <AddNodeRoutine
-              id={id}
-              rootDuration={data.video.duration}
-              currentTime={this.currentTime}
-              currentProgress={this.currentProgress}
-              setHandles={this.setHandles}
-              sideBarOpen={sideBarOpen}
-            />
-          )}
+              <AddNodeRoutine
+                id={id}
+                rootDuration={data.video.duration}
+                currentTime={this.currentTime}
+                currentProgress={this.currentProgress}
+                setHandles={this.setHandles}
+                sideBarOpen={sideBarOpen}
+              />
+            )}
           {data && data.connections && (
             <FlowerRenderer
               data={data.connections}
@@ -152,11 +152,11 @@ class FlowerView extends React.Component {
         <div
           className={style.metaContainer}
           style={{
-            display: nodeRoutineRunning ? 'none' : 'block',
+            display: nodeRoutineRunning ? "none" : "block",
             transform:
               dimensions.safeToMove && sideBarOpen
                 ? `translateX(${Math.floor(SIDEBAR_WIDTH)}px)`
-                : 'translateX(0)'
+                : "translateX(0)"
           }}
         >
           {data && data.connections && (
@@ -186,45 +186,53 @@ class FlowerView extends React.Component {
           selectedPetal &&
           selectedPetal !== data.id &&
           !nodeRoutineRunning &&
-          (session.role === 'admin' ||
+          (session.role === "admin" ||
             session.id === selectedPetalData.user.id) && [
-              <div
-            key='edit'
-            className={style.editPetal}
-            onClick={this.props.startEditNodeRoutine}
-          >
-            <img src='/icons/Btn_Edit.svg' alt='Edit Petal Button' width={20} />
-          </div>,
-              <div
-            key='delete'
-            className={style.deletePetal}
-            onClick={this.delete}
-          >
-            <img alt='Delete Petal Button' src='/icons/Btn_Delete.svg' width={15} />
-          </div>
-        ]}
+            <div
+              key="edit"
+              className={style.editPetal}
+              onClick={this.props.startEditNodeRoutine}
+            >
+              <img
+                src="/icons/Btn_Edit.svg"
+                alt="Edit Petal Button"
+                width={20}
+              />
+            </div>,
+            <div
+              key="delete"
+              className={style.deletePetal}
+              onClick={this.delete}
+            >
+              <img
+                alt="Delete Petal Button"
+                src="/icons/Btn_Delete.svg"
+                width={15}
+              />
+            </div>
+          ]}
         <div>{session.authenticated && <ActionButtonSimple size={45} />}</div>
       </div>
-    )
+    );
   }
 }
 
 FlowerView.propTypes = {
   id: PropTypes.string.isRequired,
   sideBarOpen: PropTypes.bool.isRequired
-}
+};
 
-function mapStateToProps (state) {
-  const { flowerData, session, dimensions, globals } = state
-  return { flowerData, session, dimensions, globals }
+function mapStateToProps(state) {
+  const { flowerData, session, dimensions, globals } = state;
+  return { flowerData, session, dimensions, globals };
 }
 
 const mapDispatchToProps = {
   getFlowerData,
   startEditNodeRoutine
-}
+};
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withRouter(FlowerView))
+)(withRouter(FlowerView));
--- src/components/Navigation/FlowerItem.js
+++ src/components/Navigation/FlowerItem.js
@@ -1,46 +1,46 @@
-import React from "react";
-import propTypes from "prop-types";
-import classnames from "classnames";
-import moment from "moment";
-import { connect } from "react-redux";
-import { MdEdit, MdClear } from "react-icons/md";
-import { toast } from "react-toastify";
+import React from 'react'
+import propTypes from 'prop-types'
+import classnames from 'classnames'
+import moment from 'moment'
+import { connect } from 'react-redux'
+import { MdEdit, MdClear } from 'react-icons/md'
+import { toast } from 'react-toastify'
 // import Eye from "/icons/views.svg";
-import { listFlowers } from "../../state/flowerList/actions";
-import { startEditFlowerRoutine } from "../../state/globals/actions";
+import { listFlowers } from '../../state/flowerList/actions'
+import { startEditFlowerRoutine } from '../../state/globals/actions'
 
-import style from "./FlowerItem.module.css";
+import style from './FlowerItem.module.css'
 
 class FlowerItem extends React.Component {
   delete = e => {
-    const { title, id } = this.props;
-    e.preventDefault();
+    const { title, id } = this.props
+    e.preventDefault()
     if (window.confirm(`Are you sure you want to delete ${title}?`)) {
       fetch(`${process.env.REACT_APP_SERVER_URL}/api/flower`, {
-        credentials: "include",
-        method: "DELETE",
+        credentials: 'include',
+        method: 'DELETE',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({ id })
       })
         .then(response => {
           if (response.ok) {
-            return response;
+            return response
           } else {
-            throw new Error("failed");
+            throw new Error('failed')
           }
         })
-        .then(() => toast.success("Flower successfully deleted"))
+        .then(() => toast.success('Flower successfully deleted'))
         // TODO: Why does reloading the flower instantly after deleting cause wrong responses?
         .then(setTimeout(this.props.listFlowers, 300))
         .catch(() => {
-          toast.error("Flower could not be deleted.");
-        });
+          toast.error('Flower could not be deleted.')
+        })
     }
   };
 
-  render() {
+  render () {
     const {
       title,
       id,
@@ -51,14 +51,14 @@ class FlowerItem extends React.Component {
       session,
       isSelected,
       sideBarOpen
-    } = this.props;
+    } = this.props
 
     return (
       <div
         className={`${style.container} ${
-          this.props.globals.selectedFlower ? style.barStyle : ""
+          this.props.globals.selectedFlower ? style.barStyle : ''
         }`}
-        style={{ background: isSelected ? "#E7E9EF" : "white" }}
+        style={{ background: isSelected ? '#E7E9EF' : 'white' }}
       >
         <div className={style.right}>
           <div
@@ -84,8 +84,8 @@ class FlowerItem extends React.Component {
             <div className={classnames(style.bottomContainerText)}>
               <img
                 className={style.icon}
-                src={process.env.PUBLIC_URL + "/icons/views.svg"}
-              />{" "}
+                src={process.env.PUBLIC_URL + '/icons/views.svg'}
+              />{' '}
               <div className={style.viewsText}>1,234</div>
             </div>
             <div className={classnames(style.date, style.itemPadding)}>
@@ -93,37 +93,37 @@ class FlowerItem extends React.Component {
             </div>
           </div>
           {session.authenticated &&
-            (session.role === "admin" || session.id === user.id) && [
+            (session.role === 'admin' || session.id === user.id) && [
               <div
-                key="edit"
-                className={classnames(style.edit)}
-                onClick={() => {
-                  this.props.startEditFlowerRoutine(id, {
-                    title,
-                    description,
-                    url: videoId
-                  });
-                }}
-              >
-                <MdEdit color="grey" />
-              </div>,
+              key='edit'
+              className={classnames(style.edit)}
+              onClick={() => {
+                this.props.startEditFlowerRoutine(id, {
+                  title,
+                  description,
+                  url: videoId
+                })
+              }}
+            >
+              <MdEdit color='grey' />
+            </div>,
               <div
-                key="delete"
-                className={classnames(style.delete)}
-                onClick={this.delete}
-              >
-                <MdClear color="grey" size="1.1em" />
-              </div>
-            ]}
+              key='delete'
+              className={classnames(style.delete)}
+              onClick={this.delete}
+            >
+              <MdClear color='grey' size='1.1em' />
+            </div>
+          ]}
         </div>
       </div>
-    );
+    )
   }
 }
 
 FlowerItem.defaultProps = {
-  description: "No description."
-};
+  description: 'No description.'
+}
 
 // FlowerItem.propTypes = {
 //   title: propTypes.string.isRequired,
@@ -134,17 +134,17 @@ FlowerItem.defaultProps = {
 //   id: propTypes.number.isRequired
 // };
 
-function mapStateToProps(state) {
-  const { session, globals } = state;
-  return { session, globals };
+function mapStateToProps (state) {
+  const { session, globals } = state
+  return { session, globals }
 }
 
 const mapDispatchToProps = {
   listFlowers,
   startEditFlowerRoutine
-};
+}
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(FlowerItem);
+)(FlowerItem)
